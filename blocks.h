0589c0b6688a1e8e7b1033c8dca73cec5c80ce1b
Deebakkarthi C R <deebakkarthicr@protonmail.com>
2020-09-24 10:03:18 +0530

    Only Neccessary blocks

--- blocks.h
+++ blocks.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"","status_clock",60,0},
-    {"","status_volume",0,1}
+	{"","status_volume",0,1},
+	{"","date",1,0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
